6ddf5af1936fd6bb50feb34ddc7cb897d744c655
William Artero <git@artero.dev>
2024-08-02 01:16:28 +0200

    feat(nix): added goutline package

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@
     ejson = callPackage ./nixpkgs/ejson.nix { };
     go-commitlint = callPackage ./nixpkgs/go-commitlint.nix { };
     gopium = callPackage ./nixpkgs/gopium.nix { };
+    goutline = callPackage ./nixpkgs/goutline.nix { };
     structurizr-cli = callPackage ./nixpkgs/structurizr-cli.nix { };
     structurizr-site-generatr = callPackage ./nixpkgs/structurizr-site-generatr.nix { };
   };
