331f0a452548390759def123b28ae9f385b376d6
William Artero <git@artero.dev>
2024-08-05 12:21:41 +0200

    chore: added nix shell and direnv

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  ## nix-prefetch-url --unpack <url>
+  pkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
+    sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+  }) {};
+  unstable = import (fetchTarball {
+    name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
+    # url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
+    sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
+  }) {};
+  kaizen = import ./. { inherit pkgs; };
+  inherit (pkgs) lib mkShell;
+in mkShell {
+  packages = with pkgs; [
+    git
+    go-task
+  ] ++ lib.optionals (builtins.getEnv "CI" != "") [ # CI-only
+  ] ++ lib.optionals (builtins.getEnv "CI" == "") [ # local-only
+  ];
+}
